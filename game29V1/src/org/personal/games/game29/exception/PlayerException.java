5effb0171eae27ba8f9df6ddad89f0cdce505f81
nklmthr <nklmthr@gmail.com>
2015-07-07 14:45:56 +0530

    gane29V1
    gane29V1

---
+++ game29V1/src/org/personal/games/game29/exception/PlayerException.java
@@ -0,0 +1,16 @@
+package org.personal.games.game29.exception;
+
+public class PlayerException extends Exception {
+
+
+	public PlayerException() {
+		super();
+	}
+
+	public PlayerException(String msg) {
+		super(msg);
+	}
+	public PlayerException(Exception ex, String msg){
+		super(msg, ex);
+	}
+}
