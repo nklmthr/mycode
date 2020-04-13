69389b4fa8695c2b9c21bcd43c58d36a35bce999
Nikhil Mathur <nklmthr@gmail.com>
2020-04-13 14:35:16 +0530

    aprfixx

--- sap-ariba_docker_k8s/labs/examples/shopping-cart-order-service/src/main/java/com/examples/scart/order/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examples.scart.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.examples.scart.order.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-
-}
