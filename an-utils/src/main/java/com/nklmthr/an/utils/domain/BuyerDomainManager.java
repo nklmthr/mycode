a1b7d3b00a3677e416839761e2497a0831556c95
nklmthr <nklmthr@gmail.com>
2021-02-08 10:38:02 +0530

    Delete an-utils directory

--- an-utils/src/main/java/com/nklmthr/an/utils/domain/BuyerDomainManager.java
+++
@@ -1,9 +0,0 @@
-package com.nklmthr.an.utils.domain;
-
-public class BuyerDomainManager extends DomainManager {
-	private static String buyerANCXMLDispatcherURL = "http://localhost:1114/scripts/WebObjects.dll/ANCXMLDispatcher.woa/ad/cxml";
-
-	public static String getBuyerANCXMLDispatcherURL() {
-		return buyerANCXMLDispatcherURL;
-	}
-}
