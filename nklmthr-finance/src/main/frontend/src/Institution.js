75a8fc7de907199f02da6f9c745daff2ab86500c
nklmthr <nklmthr@gmail.com>
2023-06-17 17:25:33 +0530

    UI with react with Institution

--- nklmthr-finance/src/main/frontend/src/Institution.js
+++ nklmthr-finance/src/main/frontend/src/Institution.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
+import './Institution.css';
 export default class Institution extends React.Component {
     constructor(props){
         super(props);
